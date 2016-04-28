3292cca8c3394440be0a660a7e20ddf3ac20ad3d
JasperPascual <pascual.jasper@gmail.com>
2016-04-28 13:41:32 -0700

    added login/reg functionality

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,11 @@
-// var users = require('./../controller/users.js');
+var passport = require('passport');
+var bcrypt = require('bcrypt');
 var lists = require('./../controllers/lists.js');
-
 var users = require('./../controllers/users.js');
-// var posts = require('./../controller/post.js');
-// var comments = require('./../controller/comment.js');
+
+
+
+
 
 module.exports = function(app) {
 	// GET ALL USERS
@@ -40,13 +42,64 @@ module.exports = function(app) {
 	app.post('/editTask', function(req, res){
 		lists.update(req, res);
 	})
-	app.post('/register', function(req, res) {
-        users.create(req,res);
+    app.post('/register', function(req, res, next) {
+      console.log('did a register route', req.body);
+      passport.authenticate('local-register', function(err, user, info) {
+        if (err) {
+          return res.status(401).json(info); 
+        }
+        if (!user) {
+          return res.status(401).json(info);
+        } 
+        return res.json(info);
+      })(req, res, next);
     });
 
-    app.post('/login', function(req, res) {
-        users.index(req,res);
+    app.post('/login', function(req, res, next) {
+      passport.authenticate('local-login', function(err, user, info) {
+        if (err) {
+          return res.status(401).json(info); 
+        }
+        if (!user) {
+          return res.status(401).json(info);
+        }
+        req.logIn(user, function(loginErr) {
+          if (loginErr) { return next(loginErr); }
+          console.log('success')
+          return res.json(user);
+        });    
+      })(req, res, next);
     });
+
+    app.get('/logout', function(req, res){
+      req.logout();
+      res.json(true);
+    });
+
+    //middleware to check if we're logged in
+    function isLoggedIn(req, res, next) {
+        // if user is authenticated in the session, carry on 
+        if (req.isAuthenticated()){
+          return next();
+        }
+        else{
+          console.log('unauthorized access attempt!');
+          // if they aren't redirect them to the home page
+          res.redirect('/');
+        }
+    
+    }
+
+    app.get('/profile', isLoggedIn, function(req, res){
+      User.findOne({_id: req.user._id})
+        .select('_id name local.email')
+        .exec(function(err, user){
+          if(err) console.log(err);
+          res.json(user)
+      })
+    })
+
+    
 }
 
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,11 +1,22 @@
 var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
 
+// set up a mongoose model
 var UserSchema = new mongoose.Schema({
-	firstName: String,
-	lastName: String,
-	email: String,
-	password: String
 
-})
+    name: String,
+  	local: { email: String, password: String }
+    
+});
 
-mongoose.model('User', UserSchema);
+// hash passwords
+UserSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+// checking if password is valid
+UserSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
